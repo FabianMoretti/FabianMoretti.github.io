74846ec590979cde256a2fe0152fbd9eb971ee6f
FabianMoretti <dp120793tpi@gmail.com>
2018-07-11 11:37:10 +0300

    bug fixed

--- js/main.js
+++ js/main.js
@@ -5,16 +5,27 @@ var audio;
 document.getElementById('pause').style.display = 'none';
 
 //initialize
-var initialize = (function(){
+var initialize = (function () {
+	var SONG_COUNT = 3
 	var i = 0;
-	return function(tumbler){
-		if ( tumbler == 'next') {
+	return function (tumbler) {
+		if (tumbler == 'next') {
+			var next = i + 1;
+			if (next === SONG_COUNT) {
+				i = 0;
+				return i;
+			}
 			return ++i;
 		}
-		if ( tumbler == 'prev')
+		if (tumbler == 'prev')
+			var prev = i - 1;
+		if (prev < 0) {
+			i = SONG_COUNT - 1;
+			return i;
+		}
 		return --i;
 	}
- }());
+}());
 
 initAudio(0);
 
@@ -26,6 +37,13 @@ function initAudio(index) {
 	var song = element.innerHTML;
 	//create audio object
 	audio = new Audio('media/' + song);
+	audio.addEventListener('ended', function(){
+    var next = initialize('next');
+      play.style.display = 'none';
+      pause.style.display = 'block';
+      initAudio(next);
+      audio.play();
+  });
 	for (var i = 0; i < a.length; i++) {
 		a[i].classList.add("active");
 		a[i].classList.remove("active");
@@ -34,45 +52,57 @@ function initAudio(index) {
 }
 
 //play button
-    var play = document.getElementById('play');
- 	play.onclick =function(){
- 	audio.play();
- 	play.style.display = 'none';
- 	pause.style.display = 'block';
- };
+var play = document.getElementById('play');
+play.onclick = function () {
+	audio.play();
+	play.style.display = 'none';
+	pause.style.display = 'block';
+};
 
 //pause button
-    var pause = document.getElementById('pause');
- 	pause.onclick = function(){
- 	audio.pause();
- 	play.style.display = 'block';
- 	pause.style.display = 'none';
- };
+var pause = document.getElementById('pause');
+pause.onclick = function () {
+	audio.pause();
+	play.style.display = 'block';
+	pause.style.display = 'none';
+};
 
 //Next Button
-	var next = document.getElementById('next');
-	next.onclick = function(){
-    audio.pause();
-    var next = initialize('next');
-    play.style.display = 'none';
- 	pause.style.display = 'block';
-    initAudio(next);
- 	audio.play();
+var next = document.getElementById('next');
+next.onclick = function () {
+	var playPromise = audio.play();
+	if (playPromise) {
+		playPromise.then(function () {
+			audio.pause();
+			var next = initialize('next');
+			play.style.display = 'none';
+			pause.style.display = 'block';
+			initAudio(next);
+			audio.play();
+		})
+	}
+
 };
 
 //Prev Button
-	var prev = document.getElementById('prev');
-	prev.onclick = function(){
-    audio.pause();
-	var prev = initialize('prev');
-    play.style.display = 'none';
- 	pause.style.display = 'block';
-    initAudio(prev);
- 	audio.play();
+var prev = document.getElementById('prev');
+prev.onclick = function () {
+	var playPromise = audio.play();
+	if (playPromise) {
+		playPromise.then(function () {
+			audio.pause();
+			var prev = initialize('prev');
+			play.style.display = 'none';
+			pause.style.display = 'block';
+			initAudio(prev);
+			audio.play();
+		})
+	}
+
 };
 
 //Volume Control
-	var volume = document.getElementById('volume');
-	volume.onchange = function(){
+var volume = document.getElementById('volume');
+volume.onchange = function () {
 	audio.volume = parseFloat(this.value / 10);
 };
